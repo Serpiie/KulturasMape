0f85a83d8929a222bec918dfce58c4b00eb184b1
Serpiie <slics.adrians@gmail.com>
2022-10-27 00:14:24 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,27 +22,6 @@
     });
     Stamen_Toner.addTo(map)
 
-    // dark map 
-    var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
-    });
-    // dark.addTo(map)
-
-    // google street 
-    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    // googleStreets.addTo(map);
-
-    //google satellite
-    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
-    // googleSat.addTo(map)
 
     var wms = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
         layers: 'geoapp:admin',
@@ -50,23 +29,6 @@
         transparent: true,
         attribution: "wms test"
     });
-
-
-
-    /*==============================================
-                        MARKER
-    ================================================*/
-    var myIcon = L.icon({
-        iconUrl: 'img/red_marker.png',
-        iconSize: [40, 40],
-    });
-    var singleMarker = L.marker([28.3949, 84.1240], { icon: myIcon, draggable: true });
-    var popup = singleMarker.bindPopup('This is the Nepal. ' + singleMarker.getLatLng()).openPopup()
-    popup.addTo(map);
-
-    var secondMarker = L.marker([29.3949, 83.1240], { icon: myIcon, draggable: true });
-
-    console.log(singleMarker.toGeoJSON())
 
 
     /*==============================================
